d702cc2d5064fecaa80ca41f07865923b6329f16
Muawia24 <amuawia666@gmail.com>
2024-09-11 17:15:41 +0300

    handled key inputs w,d,a,s ^,v,>,<, ESC

--- src/input.c
+++ src/input.c
@@ -4,18 +4,18 @@
  * handle_input - handle input and sliding along walls.
  * @player: a struct that holds player directions and coordinates.
  * @keyState: pointer to the keyboard input status.
- * Return: nothing.
+ * Return: 1 or 0.
  */
-void handle_input(Player *player, Game_env *game, const Uint8 *keyState)
+int handle_input(Player *player, Game_env *game, const Uint8 *keyState)
 {
 	double move_x = 0, move_y = 0;
 
-	if (keyState[SDL_SCANCODE_W])
+	if (keyState[SDL_SCANCODE_W] || keyState[SDL_SCANCODE_UP])
 	{
 	move_x = cos(player->angle) * PLAYER_SPEED;
 	move_y = sin(player->angle) * PLAYER_SPEED;
 	}
-	if (keyState[SDL_SCANCODE_S])
+	if (keyState[SDL_SCANCODE_S] || keyState[SDL_SCANCODE_DOWN])
 	{
 		move_x = -cos(player->angle) * PLAYER_SPEED;
 		move_y = -sin(player->angle) * PLAYER_SPEED;
@@ -29,9 +29,13 @@ void handle_input(Player *player, Game_env *game, const Uint8 *keyState)
 	if (game->map[(int)player->x][(int)(player->y + move_y)] == 0)
 		player->y += move_y;
 
-	if (keyState[SDL_SCANCODE_A])
+	if (keyState[SDL_SCANCODE_A] || keyState[SDL_SCANCODE_LEFT])
 		player->angle -= PLAYER_ROT_SPEED;
 
-	if (keyState[SDL_SCANCODE_D])
+	if (keyState[SDL_SCANCODE_D] || keyState[SDL_SCANCODE_RIGHT])
 		player->angle += PLAYER_ROT_SPEED;
+	if (keyState[SDL_SCANCODE_ESCAPE])
+		return (0);
+	
+	return (1);
 }
